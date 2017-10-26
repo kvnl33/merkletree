07d89bc71b975c93d5203fc710adc6dead171270
Kevin Lee <kvn5818@gmail.com>
2017-10-26 00:04:01 -0500

    Scan over the Monero blockchain

--- monero_merkle.py
+++ monero_merkle.py
@@ -5,10 +5,9 @@ import numpy as np
 from random import randint
 from hashlib import sha256
 
-global_idx = 0
 hash_function = sha256
 
-testblocks = []
+utxos = []
 
 tx_dict = OrderedDict()
 blocks = OrderedDict()
@@ -19,36 +18,6 @@ tx_root_hash = OrderedDict()
 
 top_root = None
 top_merkle = None
-
-class Block(object):
-    """Each node has, as attributes, references to left (l) and right (r) child nodes, parent (p),
-    and sibling (sib) node. It can also be aware of whether it is on the left or right hand side (side).
-    data is hashed automatically by default, but does not have to be, if prehashed param is set to True.
-    """
-    # The leaf node in here can be the block, and whatever is inside are the output keys generated
-    __slots__ = ['block_hash', 'transactions']
-    def __init__(self):
-        self.block_hash=None
-        self.transactions=[]
-
-class Transaction(object):
-    """Each node has, as attributes, references to left (l) and right (r) child nodes, parent (p),
-    and sibling (sib) node. It can also be aware of whether it is on the left or right hand side (side).
-    data is hashed automatically by default, but does not have to be, if prehashed param is set to True.
-    """
-    # The leaf node in here can be the block, and whatever is inside are the output keys generated
-    __slots__ = ['tx_hash', 'outputs']
-    def __init__(self):
-        self.tx_hash=None
-        self.outputs=[]
-
-    def generate(self):
-        self.tx_hash = id_generator(size=32)
-        cap = np.random.randint(10)+1
-        for x in range(0,cap):
-            global global_idx
-            self.outputs.append((id_generator(size=11),global_idx))
-            global_idx+=1
 
 def find_ge(my_array, target):
     '''Find smallest item greater-than or equal to key.
@@ -65,90 +34,72 @@ def find_ge(my_array, target):
 def find_nearest_above(my_array, target):
 	return min(filter(lambda y: y >= target,my_array))
 
-#	random string generator for our blocks, this will emulate the outputs in our transaction
-#	this is used for testsing purposes only
-#random string generator for our blocks 
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+def read_in_blocks():
+    '''Read in the blocks from the database containing all the RingCT outputs
+    It will be in the format of a tuple
+    '''
+    conn = sqlite3.connect('/home/yorozuya/rct_output_10_23_2017.db')
+    c_1 = conn.cursor()
+    c_1.execute('''SELECT block_hash, tx_hash, outkey, idx FROM out_table ORDER BY idx LIMIT 20''')
+    global utxos
+    utxos = c_1.fetchall()
 
-#	process a transaction and transform it into a Merkle tree
-#	The root of the Merkle tree will contain the greatest global index value
-def tx_to_merkle(tx):
-    new_t = MerkleTree(leaves=tx.outputs)
-    # build the tree
-    new_t.build()
-    return new_t
+def block_to_merkle(block_outkeys):
+    block_merkle_leaves=[]
+    block_hash = block_outkeys[0][0]
+    assert all(item[0] == block_hash for item in block_outkeys)
+    while block_outkeys:
+        curr_tx_hash = block_outkeys[0][1]
+        tx_outkeys = []
+        while block_outkeys[0][1] == curr_tx_hash:
+            tx_outkeys.append(block_outkeys.pop(0))
+            if not block_outkeys:
+                break
+        block_merkle_leaves.append(tx_to_merkle(tx_outkeys))
+    block_merkle = MerkleTree(leaves=block_merkle_leaves)
+    block_merkle.build()
+    blocks[block_hash] = block_merkle
+    block_root_hash[block_hash] = codecs.encode(block_merkle.root.val, 'hex_codec')
+    return (block_hash, block_merkle.root.idx)
 
-# 	Add a block into the global Merkle tree, let this use random data first 
-def block_to_merkle(blk):
-    tx_leaves = []
-    if isinstance(blk, Block):
-        for tx in blk.transactions:
-            newtree = tx_to_merkle(tx)
-            k = newtree.root
-            tx_dict[tx.tx_hash] = newtree
-            tx_root_hash[tx.tx_hash] = codecs.encode(k.val, 'hex_codec')
-            tx_leaves.append((tx.tx_hash,k.idx))
+def tx_to_merkle(tx_outkeys):
+    tx_hash = tx_outkeys[0][1]
+    assert all(item[1] == tx_hash for item in tx_outkeys)
+    tx_merkle_leaves = [(item[2], item[3]) for item in tx_outkeys]
+    tx_merkle = MerkleTree(leaves=tx_merkle_leaves)
+    tx_merkle.build()
+    tx_dict[tx_hash] = tx_merkle
+    tx_root_hash[tx_hash] = codecs.encode(tx_merkle.root.val, 'hex_codec')
+    return (tx_hash, tx_merkle.root.idx)
 
-        blk_merkle_tree = MerkleTree(leaves=tx_leaves)
-        blk_merkle_tree.build()
-        blk_root = blk_merkle_tree.root
-        blocks[blk.block_hash] = blk_merkle_tree
-        block_root_hash[blk.block_hash] = codecs.encode(blk_root.val, 'hex_codec')
-
-        return (blk.block_hash,blk_root.idx)
-    else:
-        raise TypeError("Input must be a block!")
-
-# codecs.encode(k.val, 'hex_codec')
-
-# used for testing, we randomly generate a block with transactions in it
-def add_block():
-    newblk = Block()
-    newblk.block_hash = id_generator(size=10)
-    cap = np.random.randint(10)+1
-    for x in range(0,cap):
-        newtx = Transaction()
-        newtx.generate()
-        newblk.transactions.append(newtx)
-    return newblk
-
-def generate_blocks():
-    limit = np.random.randint(100)+1
-    for x in range(0,limit):
-        newblk = add_block()
-        testblocks.append(newblk)
-
-def scan_over():
+def scan_over_new_blocks():
+    '''Scan over the utxos, distinguishing new blocks
+    We will use block hash to distinguish new blocks'''
+    print utxos 
     top_merkle_leaves=[]
-    while testblocks:
-        top_merkle_leaves.append(block_to_merkle(testblocks.pop(0)))
+    while utxos:
+        curr_block_hash = utxos[0][0]
+        block_outkeys = []
+        while utxos[0][0] == curr_block_hash:
+            block_outkeys.append(utxos.pop(0))
+            if not utxos:
+                break
+        top_merkle_leaves.append(block_to_merkle(block_outkeys))
+    global top_merkle
     top_merkle = MerkleTree(leaves = top_merkle_leaves)
     top_merkle.build()
-    return top_merkle
-
-# when there is a new block, process the block to a Merkle tree and add to the main tree
-# return the 
-def scan_new_block(tm, blk):
-    new_entry = block_to_merkle(blk)
-    if isinstance(tm, MerkleTree):
-        tm.add_adjust(new_entry)
-    else:
-        raise TypeError("Argument must be a Merkle Tree!")
-
-def main():
-    generate_blocks()
-    global top_merkle
-    top_merkle = scan_over()
-    # client will have access to the top_root, which it will store
     global top_root
     top_root = (codecs.encode(top_merkle.root.val, 'hex_codec'), top_merkle.root.idx)
+
+def main():
+    read_in_blocks()
+    scan_over_new_blocks()
+
     print top_root
-    
     req_gidx = np.random.randint(top_root[1])+1
     print req_gidx
 
-    found_block, blk_idx = find_ge([(leaf.data,leaf.idx) for leaf in top_merkle.leaves], req_gidx)
+    found_block, blk_idx = find_ge([(leaf.data, leaf.idx) for leaf in top_merkle.leaves], req_gidx)
     # write proof for block here
     blk_proof = top_merkle.get_proof(blk_idx)
     if check_proof(blk_proof) == top_root[0]:
