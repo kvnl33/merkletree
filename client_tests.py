80b3128b142a6e3cd2abf7784b13aeb1c3f7d5be
Kevin Lee <kvn5818@gmail.com>
2017-11-22 11:19:51 -0600

    Added proof test

--- client_tests.py
+++ client_tests.py
@@ -20,6 +20,19 @@ def query_test(server):
     elapsed = end - start
     return elapsed
 
+def proof_test(server):
+    if server == server1:
+        top = t1_root
+    else:
+        top = t2_root
+    req_gidx = np.random.randint(0,top[1]+1)
+    found_out, found_proof = client.get_output(server, req_gidx)
+    start = time.time()
+    client.check_path(found_out, found_proof, top)
+    end = time.time()
+    elapsed = end - start
+    return elapsed
+
 def main():
     if first_arg=="query":
         print "Testing 1000 queries..."
@@ -28,6 +41,13 @@ def main():
             for x in range(0,500):
                 avg.append(query_test(server))
         print "Average query time for 1000 trials is %.6f seconds."%(np.average(avg))
+    elif first_arg=="proof":
+        avg = []
+        print "Testing 1000 proofs..."
+        for server in [server1, server2]:
+            for x in range(0,500):
+                avg.append(query_test(server))
+        print "Average time to check proof for 1000 trials is %.6f seconds."%(np.average(avg))
     else:
         print "Please provide an argument"
         
